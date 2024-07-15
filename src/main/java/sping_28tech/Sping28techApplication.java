1e880b3128e19dfc8cbe25ada957db65bf6e8b09
luc12 <danunike687@gmail.com>
2024-07-15 23:34:17 +0700

    update 11h34

---
+++ src/main/java/sping_28tech/Sping28techApplication.java
@@ -0,0 +1,13 @@
+package sping_28tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sping28techApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sping28techApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sping_28tech/Sping28techApplicationTests.java
@@ -0,0 +1,13 @@
+package sping_28tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sping28techApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
